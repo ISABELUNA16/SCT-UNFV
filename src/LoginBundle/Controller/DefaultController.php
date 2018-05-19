31e5f134670e360a9ac28f2411a34a042048c1dd
Isabel Luna <isabel1625.luna@gmail.com>
2018-05-19 07:36:19 -0500

    mod de usuario terinado - adm recepcion de incidencias - login terminado

---
+++ src/LoginBundle/Controller/DefaultController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace LoginBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('LoginBundle:Default:index.html.twig');
+    }
+
+    public function usuarioPrincipalAction(){
+      
+        return $this->render('LoginBundle:Default:usuario_principal.html.twig' );
+    }
+
+    public function administradorPrincipalAction(){
+
+    	return $this->render('LoginBundle:Default:admin_principal.html.twig');
+    }
+
+}
